ec1b01b763e27808335782e941267c6665f7d417
Saumya Jaiswal <24jaiswalsaumya@gmail.com>
2023-03-04 16:22:46 +0530

    axios

--- main.js
+++ main.js
@@ -1,49 +1,174 @@
+// AXIOS GLOBALS 
+axios.defaults.headers.common['X-Auth-Token']='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  axios({
+    method:'get',
+    url:'https://jsonplaceholder.typicode.com/todos',
+    params:{
+        _limit:5,
+    }
+  }).then(res=>showOutput(res))
+    .catch(err=>console.log(err))
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+
+  // way 1
+  axios({
+    method:'post',
+    url:'https://jsonplaceholder.typicode.com/posts',
+    data:{
+        title:'New Post',
+        completed: false,
+
+
+    }
+  }).then(res=>showOutput(res))
+    .catch(err=>console.log(err))
+
+  // way 2
+  axios.post('https://jsonplaceholder.typicode.com/posts',{
+    title:'New Post',
+    completed: false,
+  })
+    .then(res=>showOutput(res))
+    .catch(err=>console.log(err))
+
+
+  
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+
+  axios.put('https://jsonplaceholder.typicode.com/posts/1',{
+    title:'Updated todo',
+    completed: true,
+  })
+    .then(res=>showOutput(res))
+    .catch(err=>console.log(err))
+
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  axios.delete('https://jsonplaceholder.typicode.com/posts/1')
+    .then(res=>showOutput(res))
+    .catch(err=>console.log(err))
+  
 }
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  axios.all([
+      axios.get('https://jsonplaceholder.typicode.com/todos'),
+      axios.post('https://jsonplaceholder.typicode.com/posts')
+  ])
+    .then(res=>{
+      console.log(res[0]),
+      console.log(res[1])
+    })
+    .catch(err=>console.log(err))
+  
 }
 
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+  const config = {
+    header:{
+      'Content-Type':'application/JSON',
+      Authorisation : 'someToken'
+    }
+  }
+
+  axios.post('https://jsonplaceholder.typicode.com/posts',{
+    title:'New Post',
+    completed: false,
+  },config)
+    .then(res=>showOutput(res))
+    .catch(err=>console.log(err))
+
+
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const option={
+    method:'post',
+    url:'https://jsonplaceholder.typicode.com/todos',
+    data:{
+      title: 'Hello World'
+    },
+    transformResponse: axios.defaults.transformResponse.concat(data=>{
+      data.title=data.title.toUpperCase();
+      return data;
+    })
+  }
+  axios(option).then(res=>showOutput(res))
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios.get('https://jsonplaceholder.typicode.com/todoss')
+  .then(res=>showOutput(res))
+  .catch(err=>{
+    if (err.response){
+      console.log(err.response.data),
+      console.log(err.response.status),
+      console.log(err.response.headers)
+
+    if (err.response.status === 404){
+      alert('Page not found')
+    }
+
+  }
+  else if (err.request){
+     console.log(err.request)
+  }
+  else{
+    console.log(err.message)
+  }
+
+  })
+
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+  const source = axios.CancelToken.source();
+  axios.get('https://jsonplaceholder.typicode.com/todos',{
+    cancelToken:source.token
+  })
+   
+  .then(res=>showOutput(res))
+  .catch(thrown=>{
+    if(axios.isCancel(thrown)){
+      console.log('Request Cancelled',thrown.message);
+    }
+  });
+  if (true){
+    source.cancel('Request Cancelled')
+  }
 }
 
+// AXIOS INSTANCE 
+const axiosInstance = axios.create({
+  baseURL:'https://jsonplaceholder.typicode.com'
+})
+axiosInstance.get('/comments?_limit=5').then(res=>showOutput(res))
+
+
 // INTERCEPTING REQUESTS & RESPONSES
+
+axios.interceptors.request.use(config=>{
+  console.log(`${config.method.toUpperCase()} request send to ${config.url} at ${new Date().getTime()}`);
+
+  return config;
+
+})
 
 // AXIOS INSTANCES
 
